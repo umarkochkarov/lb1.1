1371f15a3d0c742e05d43d1b43f67e6f4a5c40b2
umarkochkarov <kochkarov-u@mail.ru>
2022-09-04 22:07:00 +0300

    7com

--- Programm/main.cpp
+++ Programm/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Hello world!" << endl;
     cout << "My name is Umar" << endl;
     cout << "I am 19 years old" << endl;
+    cout << "I am from Karachay" << endl;
     return 0;
 }
